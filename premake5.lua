5a3ea077e13676133e49545575cccd44c4d182a5
Paul Hickman <PaulHickman123@googlemail.com>
2019-05-31 17:10:09 +0100

    > Updated premake to use static runtime

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "imgui"
     kind "StaticLib"
     language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
     
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -20,10 +22,12 @@ project "imgui"
     }
     
 	filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
 		systemversion "latest"
-		cppdialect "C++17"
-        staticruntime "On"
 
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+	
+	filter "configurations:Release"
+		runtime "Debug"
+		symbols "on"
